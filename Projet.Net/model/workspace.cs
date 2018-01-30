7c113496670fa2267bafa07523191cefadd239a5
Daniel Payet <danieldu974@gmail.com>
2018-01-30 09:48:07 +0100

    ajout de la classe workspace

---
+++ Projet.Net/model/workspace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet.Net.model
+{
+    class Workspace
+    {
+        private static Workspace instance = null;
+        private string chemin;
+        private Workspace()
+        {
+        }
+
+        public static Workspace getInstance()
+        {
+            if (instance == null)
+            {
+                return new Workspace();
+            }
+            else
+            {
+                return instance;
+            }
+        }
+    }
+}
